d810ca28a7147a451aaf5442919a806d8642b4fb
Chaitanya Kumar <chaitanya4real@gmail.com>
2021-10-11 20:22:43 +0530

    used async/await for auth

--- server/app.js
+++ server/app.js
@@ -2,15 +2,15 @@ const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT;
-
 dotenv.config({ path: "./config.env" });
 require("./db/conn");
+
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.use(require("./router/auth"));
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
 });
--- server/router/auth.js
+++ server/router/auth.js
@@ -8,30 +8,78 @@ router.get("/", (req, res) => {
   res.send(`hello world from router js`);
 });
 
-router.post("/register", (req, res) => {
+require("../model/userSchema");
+// PROMISES
+// router.post("/register",(req, res) => {
+//   const { name, email, phone, work, password, cpassword } = req.body;
+//   if (!name || !email || !phone || !work || !password || !cpassword) {
+//     return res.status(400).json({ error: "Please fill all the data" });
+//   }
+
+//   User.findOne({ email: email })
+//     .then((userExist) => {
+//       if (userExist) {
+//         return res.status(400).json({ error: "Email already exists" });
+//       }
+//       const user = new User({ name, email, phone, work, password, cpassword });
+//       user
+//         .save()
+//         .then(() => {
+//           res.status(201).json({ message: "User registered succesfully" });
+//         })
+//         .catch((err) => {
+//           res.status(500).json({ error: "Failed to register" });
+//         });
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+router.post("/register", async (req, res) => {
   const { name, email, phone, work, password, cpassword } = req.body;
   if (!name || !email || !phone || !work || !password || !cpassword) {
     return res.status(400).json({ error: "Please fill all the data" });
   }
 
-  User.findOne({ email: email })
-    .then((userExist) => {
-      if (userExist) {
-        return res.status(400).json({ error: "Email already exists" });
-      }
-      const user = new User({ name, email, phone, work, password, cpassword });
-      user
-        .save()
-        .then(() => {
-          res.status(201).json({ message: "User registered succesfully" });
-        })
-        .catch((err) => {
-          res.status(500).json({ error: "Failed to register" });
-        });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  try {
+    const userExist = await User.findOne({ email: email });
+    if (userExist) {
+      return res.status(422).json({ error: "Email already exists" });
+    }
+
+    const user = new User({ name, email, phone, work, password, cpassword });
+
+    await user.save();
+    
+    res.status(201).json({ message: "User registered succesfully" });
+    
+    } catch (err) {
+    console.log(err);
+  }
+});
+
+//login route
+router.post('/signin', async (req,res)=>{
+    // console.log(req.body);
+    // res.json({message: "nice"});
+    try{
+        const {email,password} = req.body;
+        if(!email || !password){
+            return res.status(200).json({error: "Please fill the data"});
+        }
+        const userLogin = await User.findOne({email:email});
+
+        if(!userLogin){
+            res.json({error:"User doesn't exist"});
+        }
+        else{
+            res.json({message:"You signed in succesfully"});
+        }
+        
+    }catch(err){
+        console.log(err);
+    }
 });
 
 module.exports = router;
